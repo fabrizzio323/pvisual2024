9e87e160edd9cb237e03d566ce887c620427d387
Fabrizio <Fabrizio@cyber>
2024-04-10 19:07:23 -0300

    Ejercicio 2 realizado Atte: Fabrizio Luciano Armada

---
+++ TP1_FabrizioLucianoArmada/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  String pais, numeroTelefono;
+	        int edad;
+	        double alturaEdificio, precioProducto, coseno;
+		    pais = "Argentina";
+	        numeroTelefono = "388-4048557";
+	        edad = 20;
+	        alturaEdificio = 52.5;
+	        precioProducto = 14854.85;
+	        coseno = Math.cos(0.5);
+	        System.out.println("Pais:" + pais + " Numero telefonico:" + numeroTelefono + " Edad:" + edad + " Altura de un edificio:" + alturaEdificio + "M" + " Precio: $" + precioProducto + " Coseno:" + coseno);
+
+	}
+
+}
